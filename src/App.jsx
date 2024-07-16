25c592a16c208c605ca615dff73ccfa7b9941869
SanjayUG <ganjekarsanjay6@gmail.com>
2024-07-16 18:27:53 +0530

    feat: learnt to add a component(added demo)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 
-
 function App() {
-
-
   return (
     <>
       <h1>HARE KRISHNA</h1>
+      <p>This is App page</p>
     </>
   )
 }
---
+++ src/Demo.jsx
@@ -0,0 +1,10 @@
+function Demo() {
+  return (
+    <>
+      <h1>HARE RAMA</h1>
+      <p>This is Demo page</p>
+    </>
+  );
+}
+
+export default Demo;
